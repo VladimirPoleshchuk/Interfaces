42f1b890d8227568ee1f785730cdd14c2a99258d
Vladimir Poleshchuk <polvlad1m@yandex.ru>
2023-01-24 14:30:07 +0700

    Добавил Main

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿namespace Interfaces
+{
+    class Program
+    {
+        static void Main()
+        {
+            Quadcopter copter = new Quadcopter();                      
+
+            foreach (var item in copter.GetComponents())
+            {
+                Console.WriteLine(item);
+            }
+
+            copter.Charge();
+
+            Console.WriteLine(((IFlyingRobot)copter).GetRobotType());
+        }
+    }
+}
